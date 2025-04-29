ff9f20b51691f223861a7fb38aa578476ea57d81
Abhranil Pal <82752393+abhraliteyaars@users.noreply.github.com>
2025-04-29 16:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,5 +152,5 @@ def download():
     return send_file(results_file_path, as_attachment=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
